6e315b4480c9bedacb0ea137169285c8997b19ce
the-noobest-pro <87678768+the-noobest-pro@users.noreply.github.com>
2021-09-05 06:23:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+ARQ_API_KEY = os.getenv("ARQ_API_KEY", "NFFEQV-BDCZXO-RBVUDJ-IIOBHK-ARQ")
+ARQ_API_URL = "https://thearq.tech/"
