2fd44854c9a71185b5e30df6b973abbe7b47cb5e
the-noobest-pro <87678768+the-noobest-pro@users.noreply.github.com>
2021-09-05 06:27:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,15 @@ import logging
 import requests
 import aiohttp
 import json
+from io import BytesIO
+from aiohttp import ClientSession
+from traceback import format_exc
+from Python_ARQ import ARQ
 from youtube_dl import YoutubeDL
 from pyrogram import filters, Client, idle
 from youtubesearchpython import SearchVideos
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, ARQ_API_KEY, ARQ_API_URL
 
 
 # logging
@@ -21,6 +25,32 @@ bot = Client(
    api_hash=API_HASH,
    bot_token=BOT_TOKEN,
 )
+
+aiohttpsession = ClientSession()
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+async def quotify(messages: list):
+    response = await arq.quotly(messages)
+    if not response.ok:
+        return [False, response.result]
+    sticker = response.result
+    sticker = BytesIO(sticker)
+    sticker.name = "sticker.webp"
+    return [True, sticker]
+
+
+def getArg(message: Message) -> str:
+    arg = message.text.strip().split(None, 1)[1].strip()
+    return arg
+
+
+def isArgInt(message: Message) -> bool:
+    count = getArg(message)
+    try:
+        count = int(count)
+        return [True, count]
+    except ValueError:
+        return [False, 0]
 
 
 @bot.on_message(filters.command("start") & ~filters.edited)
@@ -102,6 +132,66 @@ async def song(_, message):
         os.remove(thumb)
     except Exception as e:
         print(e)
+
+@bot.on_message(filters.command(["q", "quote"]) & ~filters.edited)
+async def quotly_func(client, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("`Reply To a Message To Quote It 🙄!`")
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "`Replied Message didn't contain any text! Please Reply To a Text Message 🥺!`"
+        )
+    m = await message.reply_text("`Quoting Messages...`")
+    if len(message.command) < 2:
+        messages = [message.reply_to_message]
+
+    elif len(message.command) == 2:
+        arg = isArgInt(message)
+        if arg[0]:
+            if arg[1] < 2 or arg[1] > 10:
+                return await m.edit("`Argument must be between 2-10`")
+            count = arg[1]
+            messages = await client.get_messages(
+                message.chat.id,
+                [
+                    i
+                    for i in range(
+                        message.reply_to_message.message_id,
+                        message.reply_to_message.message_id + count,
+                    )
+                ],
+                replies=0,
+            )
+        else:
+            if getArg(message) != "r":
+                return await m.edit(
+                    f"Incorrect Argument, Pass `r` or `INT` \n**Example:** `/q 2`"
+                )
+            reply_message = await client.get_messages(
+                message.chat.id,
+                message.reply_to_message.message_id,
+                replies=1,
+            )
+            messages = [reply_message]
+    else:
+        await m.edit(
+            f"Incorrect Argument, Pass `r` or `INT` \n**Example:** `/q 2`"
+        )
+        return
+    try:
+        sticker = await quotify(messages)
+        if not sticker[0]:
+            await message.reply_text(sticker[1])
+            return await m.delete()
+        sticker = sticker[1]
+        await message.reply_sticker(sticker)
+        await m.delete()
+        sticker.close()
+    except Exception as e:
+        await m.edit(f"`Something wrong happened while quoting messages. This error usually happens when there's a message containing something other than text.`")
+        e = format_exc()
+        print(e)
+
 
 bot.start()
 idle()
